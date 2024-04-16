a2c786f6f9177bddddadb2a1a699bf5d613a2b26
Getaneh48 <getaneh11@gmail.com>
2024-04-16 18:45:11 +0000

    added a logic to load a type of storage based on env values

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,28 @@
 #!/usr/bin/python3
-"""This module instantiates an object of class FileStorage"""
+"""
+This modules determines the type of storage and instantiate and initialize
+it accordingly
+"""
+from os import environ
 from models.engine.file_storage import FileStorage
+from models.engine.db_storage import DBStorage
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
 
-storage = FileStorage()
-storage.reload()
+if environ['HBNB_TYPE_STORAGE'] == 'db':
+    # storage type is database storage
+    from models.engine.db_storage import DBStorage
+    storage = DBStorage()
+    storage.reload()
+
+else:
+    # storage type is file storage
+    from models.engine.file_storage import FileStorage
+    storage = FileStorage()
+    storage.reload()
