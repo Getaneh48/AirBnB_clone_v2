b9da665dce22ee9b4df174d7b5415fb7ca76e9a9
Getaneh48 <getaneh11@gmail.com>
2024-05-03 20:16:21 +0000

    service restart logic changed

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -15,5 +15,5 @@ sudo echo "<html>
 </html>" | sudo tee /data/web_static/releases/test/index.html
 sudo chown -R ubuntu:ubuntu /data
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-sudo sed -i '53i \\tlocation \/hbnb_static {\n\t\t alias /data/web_static/current;\n\t}' /etc/nginx/sites-available/default
+sudo sed -i '54i \\tlocation \/hbnb_static {\n\t\t alias /data/web_static/current;\n\t}' /etc/nginx/sites-available/default
 sudo service nginx restart
