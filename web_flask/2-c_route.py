dbe81c4711c215a501bcff812ddb09a1969993d0
Getaneh48 <getaneh11@gmail.com>
2024-05-17 10:25:11 +0300

    added the route /c/<text>

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3a
+"""
+a module that starts a flask app and render routes
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    """landing url"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb')
+def hbnb():
+    """ renders the route /hbnb """
+    return "HBNB"
+
+
+@app.route('/c/<text>')
+def c(text):
+    """ handles a route /c with some given value """
+    return f"C {text.replace('_', ' ')}"
+
+
+if __name__ == '__main__':
+    app.url_map.strict_slashes = False
+    app.run(host='0.0.0.0', port=5000)
