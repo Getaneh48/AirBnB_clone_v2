fc29481476ab004e440c3446b6e9f16e29a095f3
Getaneh48 <getaneh11@gmail.com>
2024-05-18 20:28:30 +0000

    function docs added

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -17,11 +17,13 @@ def storage_close(arg=None):
 
 @app.route('/states_list')
 def states_list():
+    """lists all state"""
     all_states = storage.all(State)
     states = sorted(all_states.values(), key=lambda state: state.name)
     return render_template('7-states_list.html', states=states)
 
 
 if __name__ == '__main__':
+    """main function runs flask app"""
     app.url_map.strict_slashes = False
     app.run(host='0.0.0.0', port=5000)
